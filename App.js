7bf577dafda38e31883606af150d70ef19cafbfa
Kirua <yo44prg@icloud.com>
2021-10-18 11:23:56 +0200

    feat: first commit

--- App.js
+++ App.js
@@ -1,21 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { View } from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import LoginView from "./views/Login.js";
+
+import HomeView from "./views/Home.js";
+import AboutView from "./views/About.js";
+
+
+const Tab = createMaterialBottomTabNavigator();
+
+const userIsConnected = false;
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  if (userIsConnected) {
+    return (
+      <NavigationContainer>
+        <Tab.Navigator 
+          activeColor="#fff"
+          inactiveColor="#a8a8a4"
+          barStyle={{ backgroundColor: '#3a5790' }}
+        >
+          <Tab.Screen 
+            name="Home" 
+            component={HomeView}
+            options={{
+              tabBarLabel: 'Home',
+              tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons name="home" color={color} size={26} />
+              ),
+            }} 
+          />
+          <Tab.Screen 
+            name="About" 
+            component={AboutView}
+            options={{
+              tabBarLabel: 'About',
+              tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons name="trending-up" color={color} size={26} />
+              ),
+            }} 
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    );
+  } else {
+    return(
+      <View>
+        <LoginView/>
+      </View>
+    );
+  }
+  
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ views/Login.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+import { View, Text } from "react-native";
+import { Header } from 'react-native-elements';
+
+export default function LoginView() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center'}}>
+      <Text>Login</Text>
+    </View>
+  );
+}
